b31c0552d386a4e3d049d59e900f98c2c42e13ec
ddheer29 <divyangdheer11@gmail.com>
2024-12-22 23:28:52 +0530

    FCM Token generated for both android and ios

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import { Dimensions, Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import AnimatedSearchBar from './src/AnimatedSearchBar';
 import SwipeAnimation from './src/SwipeAnimation';
 import AnimatedSlider from './src/AnimatedSlider';
 import Navigation from './src/Navigation/Navigation';
 import { Provider } from 'react-redux';
 import store from './src/redux/store/Store';
+import { requestUserPermission } from './src/utils/notificationService';
 
 
 const App = () => {
+
+  useEffect(() => {
+    requestUserPermission();
+  }, [])
+
   return (
     <Provider store={store}>
       <Navigation />
---
+++ src/utils/notificationService.js
@@ -0,0 +1,35 @@
+import messaging from '@react-native-firebase/messaging';
+import {PermissionsAndroid, Platform} from 'react-native';
+
+export async function requestUserPermission() {
+  if (Platform.OS === 'android' && Platform.Version >= 33) {
+    const granted = PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      getFCMToken();
+    } else {
+      console.log('Permission denied');
+    }
+  }
+
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+    getFCMToken();
+  }
+}
+
+const getFCMToken = async () => {
+  try {
+    await messaging().registerDeviceForRemoteMessages();
+    const token = await messaging().getToken();
+    console.log('🚀 ~ getFCMToken ~ token:', token);
+  } catch (error) {
+    console.log(error);
+  }
+};
